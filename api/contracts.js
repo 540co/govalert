67041197a1f31d1a5c680d9f7f12b5db02aad157
Martin Berlove <martin@mberlove.com>
2019-04-24 10:31:16 -0400

    Made the contracts endpoint accept geo bounds.

--- api/contracts.js
+++ api/contracts.js
@@ -1,77 +1,122 @@
 exports = function(payload) {
   
-  /*
-  Data in:
-  - Award ID
-  - Dollars obligated
-  - Primary place of performance, possibly in multiple formats
-  - Awarding agency code
-  - Awarding agency name
-  
-  Data out:
-  - Award ID (?)
-  - Dollars obligated
-  - Primary place of performance, in location format requested or a default
-  */
-  
-  // Preserving the mocked data for testing / dev purposes.
-  function getFakeData() {
+  // Preserving the ability to get mocked data for testing / dev purposes.
+  function getFakeData(zip, county, state, latitude, longitude, ugc) {
     
+    // A few records to use
     var unfiltereddata = [
-      { "awardIdPiid": "INR14PD00063", "totalDollarsObligated": 53655.69, "awardingAgencyName": "DEPARTMENT OF THE INTERIOR (DOI)", "awardingAgencyCode": "014", "primaryPlaceOfPerformanceZipCode": "89005", "primaryPlaceOfPerformanceUGCCode": "NVC003", "primaryPlaceOfPerformanceState": "NV" },
-      { "awardIdPiid": "VA25716P0008", "totalDollarsObligated": 37440.00, "awardingAgencyName": "DEPARTMENT OF VETERANS AFFAIRS (VA)", "awardingAgencyCode": "036", "primaryPlaceOfPerformanceZipCode": "75216", "primaryPlaceOfPerformanceUGCCode": "TXC113", "primaryPlaceOfPerformanceState": "TX" },
-      { "awardIdPiid": "SPE3SU19F045F", "totalDollarsObligated": 475.63 , "awardingAgencyName": "DEPARTMENT OF DEFENSE (DOD)", "awardingAgencyCode": "097", "primaryPlaceOfPerformanceZipCode": "30354", "primaryPlaceOfPerformanceUGCCode": "GAC121", "primaryPlaceOfPerformanceState": "GA" }
+      {"_id":"5cb419cd20d3833856eedbbf","awardIdPiid":"123J1419C0579","parentAwardAgencyName":"","federalActionObligation":"589824.00","totalDollarsObligated":"589824.00","baseAndExercisedOptionsValue":"589824.00","currentTotalValueOfAward":"589824.00","actionDate":"2018-12-19","periodOfPerformanceStartDate":"2018-12-19 00:00:00","periodOfPerformanceCurrentEndDate":"2019-02-28 00:00:00","awardingAgencyCode":"012","awardingAgencyName":"DEPARTMENT OF AGRICULTURE (USDA)","awardingOfficeCode":"123J14","awardingOfficeName":"USDA  AMS  3J14","primaryPlaceOfPerformanceZip":"51334","primaryPlaceOfPerformanceCountyName":"EMMET","primaryPlaceOfPerformanceStateCode":"IA","primaryPlaceOfPerformanceLat":"43.404109","primaryPlaceOfPerformanceLng":" -94.805056","primaryPlaceOfPerformanceUgc":["IAC063"],"awardType":"DEFINITIVE CONTRACT","lastModifiedDate":"2018-12-26 14:16:13","recipient":{"name":"SONSTEGARD FOODS COMPANY","duns":"154549059","cageCode":"3Q8V8"}},
+      {"_id":"5cb4194220d3833856ece7be","awardIdPiid":"12RADZ19P0201","parentAwardAgencyName":"","federalActionObligation":"612.50","totalDollarsObligated":"612.50","baseAndExercisedOptionsValue":"612.50","currentTotalValueOfAward":"612.50","actionDate":"2019-03-11","periodOfPerformanceStartDate":"2019-03-11 00:00:00","periodOfPerformanceCurrentEndDate":"2019-04-11 00:00:00","awardingAgencyCode":"012","awardingAgencyName":"DEPARTMENT OF AGRICULTURE (USDA)","awardingOfficeCode":"12RADZ","awardingOfficeName":"USDA/RD/REGIONAL ACQ DIVISION","primaryPlaceOfPerformanceZip":"61356","primaryPlaceOfPerformanceCountyName":"BUREAU","primaryPlaceOfPerformanceStateCode":"IL","primaryPlaceOfPerformanceLat":"41.396035","primaryPlaceOfPerformanceLng":" -89.413241","primaryPlaceOfPerformanceUgc":["ILC011"],"awardType":"PURCHASE ORDER","lastModifiedDate":"2019-03-11 10:13:50","recipient":{"name":"'B. F. SHAW PRINTING COMPANY, THE'","duns":"089641989","cageCode":"3QFA8"}},
+      {"_id":"5cb4194220d3833856ece7ca","awardIdPiid":"80NM0018F0616","parentAwardAgencyName":"NATIONAL AERONAUTICS AND SPACE ADMINISTRATION","federalActionObligation":"500000.00","totalDollarsObligated":"247082102.82","baseAndExercisedOptionsValue":"0.00","currentTotalValueOfAward":"670513066.78","actionDate":"2019-03-11","periodOfPerformanceStartDate":"2019-03-11 00:00:00","periodOfPerformanceCurrentEndDate":"2020-09-27 00:00:00","awardingAgencyCode":"080","awardingAgencyName":"NATIONAL AERONAUTICS AND SPACE ADMINISTRATION (NASA)","awardingOfficeCode":"80NM00","awardingOfficeName":"NASA MANAGEMENT OFFICE -- JPL","primaryPlaceOfPerformanceZip":"91109","primaryPlaceOfPerformanceCountyName":"LOS ANGELES","primaryPlaceOfPerformanceStateCode":"CA","primaryPlaceOfPerformanceLat":null,"primaryPlaceOfPerformanceLng":null,"primaryPlaceOfPerformanceUgc":["CAC037"],"awardType":"DELIVERY ORDER","lastModifiedDate":"2019-03-11 13:49:35","recipient":{"name":"CALIFORNIA INSTITUTE OF TECHNOLOGY","duns":"009584210","cageCode":"80707"}}
     ];
     
+    // The fields by which one may filter
+    var fields = [[zip, "Zip"], [county, "CountyName"], [state, "StateCode"], [latitude, "Lat"], [longitude, "Lng"], [ugc, "Ugc"]];
+    
     // Perform fake filtering simulating what the query would do in mongo.
-    var filtereddata = [];
-    
     // Include any record that matches all the query parameters.
-    for (var i in unfiltereddata) {
-      if (agency) { if (unfiltereddata[i].awardingAgencyName != agency) { continue }}
-      if (state) { if (unfiltereddata[i].primaryPlaceOfPerformanceState != state) { continue } }
-      filtereddata.push(unfiltereddata[i]);
-    }
-    
-    return filtereddata;
+    return unfiltereddata.filter(function(datum) {
+      for (var f in fields) { if (fields[f][0]) { if (datum["primaryPlaceOfPerformance" + fields[f][1]] == fields[f][0]) { return true } } }
+    });
     
   } // End of getFakeData()
   
   // Grab contract data from mongo
-  function getData(ugc) {
+  function getData(zip, county, state, latitude, longitude, ugc, mock) {
+    if (mock) { return getFakeData(zip, county, state, latitude, longitude, ugc) }
     var filters = {};
     if (ugc) { filters.primaryPlaceOfPerformanceUgc = ugc }
-    return context.services.get("540-1").db("govalert").collection("contracts").find({primaryPlaceOfPerformanceUgc: ugc}).toArray();
+    return context.services.get("540-1").db("govalert").collection("contracts").find(filters).toArray();
   }
   
-  // Optional parameters for by state, by city, agency, etc.
-  var agency = payload.query.agency;
-  var state = payload.query.state;
-  var ugc = payload.query.ugc;
+  // You shouldn't need the function below.
   
-  // If mock param is set, return fake data. Otherwise, return true data.
-  var mock = payload.query.mock;
+  // maxlat, minlong, minlat, maxlong
+  function isBounded(top, left, bottom, right, lat, long) {
+    console.log("Finding [" + long + ", " + lat + "] within [" + top + ", " + left + "], [" + bottom + ", " + right + "].")
+    console.log("Top >= lat: " + (top >= lat));
+    console.log("Lat >= bottom: " + (lat >= bottom));
+    console.log("Left <= right: " + (left <= right) + " Left " + left + " right " + right);
+    console.log("Left <= long: " + (left <= long));
+    console.log("Long <= right: " + (long <= right));
+    if (top >= lat && lat >= bottom) {
+      if (left <= long && long <= right) {
+        console.log("Found bounded.")
+        return true;
+      }
+      /*else if (left > right && (left <= long || long <= right)) {
+            //console.log("Found bounded.")
+        return true;
+      }*/
+    }
+    return false;
+  }
   
-  // Mock incoming data -- transformed for output, even as mocked data, to simplify things later.
-  var basedata = (mock ? getFakeData() : getData(ugc));
+  function getBoundedData(minlat, minlong, maxlat, maxlong) {
+    /*var filters = { 
+      primaryPlaceOfPerformancePoint: {
+        $geoWithin: { 
+          $geometry: { 
+            type: "Polygon", 
+            coordinates: [[ 
+              [minlong, minlat], 
+              [maxlong, minlat], 
+              [maxlong, maxlat], 
+              [minlong, maxlat], 
+              [minlong, minlat] 
+            ]] 
+          } 
+        }
+      }
+    };*/
+    var filters = {};
+ 
+    context.services.get("540-1").db("govalert").collection("contracts").find({}).limit(100).toArray().then(result => {
+      var data = result.filter(function(datum) { return isBounded(maxlat, minlong, minlat, maxlong, Number(datum.primaryPlaceOfPerformanceLat), Number(datum.primaryPlaceOfPerformanceLng)) });
+      /*data = data.filter(function(datum) {
+        return datum.primaryPlaceOfPerformanceUgc && datum.primaryPlaceOfPerformanceUgc[0] && datum.primaryPlaceOfPerformanceUgc[0].charAt(2) == 'C';
+      });*/
+      //console.log(data[0].primaryPlaceOfPerformanceCountyName)
+      basedata = data;
+    });
+
+  }
   
+  var basedata = null;
+  var q = payload.query; 
   
-  // At this point we have the data one way or another.
-  // Currently mocked, but eventually from mongodb.
-  // The transformation into what gets sent back should be agnostic of source.
-  for (var j in basedata) { 
+  if (q.minlat && q.minlong && q.maxlat && q.maxlong) {
+    //getBoundedData(q.minlat, q.minlong, q.maxlat, q.maxlong);
     
-    // Filter out any record without a UGC code or where the UGC code's 3rd letter is not 'C'
-    basedata = basedata.filter(function(datum) {
+  }
+  else {
+    // If mock param is set, return fake data. Otherwise, return true data.
+    basedata = getData(q.zip, q.county, q.state, q.latitude, q.longitude, q.ugc, q.mock);
+    return filter(function(datum) {
       return datum.primaryPlaceOfPerformanceUgc && datum.primaryPlaceOfPerformanceUgc[0] && datum.primaryPlaceOfPerformanceUgc[0].charAt(2) == 'C';
     });
-    
-    //context.services.get("540-1").db("govalert").collection("locations").findOne({Zipcode: filtereddata[j].primaryPlaceOfPerformanceZipCode})
-    //.then(result => filtereddata[j].location = [result.Long, result.Lat])
-    
   }
   
+  const dodata = new Promise(function(resolve, reject) {
+  
+    context.services.get("540-1").db("govalert").collection("contracts").find({}).limit(100).toArray().then(result => {
+      var data = result.filter(function(datum) { return isBounded(q.maxlat, q.minlong, q.minlat, q.maxlong, Number(datum.primaryPlaceOfPerformanceLat), Number(datum.primaryPlaceOfPerformanceLng)) });
+      console.log(data);
+      basedata = data;
+    });
+  
+    resolve('fine')
+});
+
+  return dodata.then(function whenOk(response) {
+    console.log("At this point: " + basedata[0].primaryPlaceOfPerformanceLng)
+    return basedata;
+  })
+  .catch(function notOk(err) {
+    console.error(err)
+  });
+  
   // Build and return the data
-  return context.functions.execute("buildResponse", basedata, state, agency);
+ // return context.functions.execute("buildResponse", basedata, q.zip, q.county, q.state, q.latitude, q.longitude, q.ugc);
 
 };
